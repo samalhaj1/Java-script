f170ce2588d7a802dae4ef065e33bb96ed17d52c
samalhaj1 <>
2021-06-02 15:29:11 +0000

    adding new repo

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("I am glad that you are here welcome")
+var x = prompt("which kind of birds do you prefer?")
+if(x == 'amirican') {alert("you are welcome to my website")}
+else{
+  alert("go and search in another website")
+ }
